fed0bc317daf1dae2684a1fc4713d8fb40e8d69f
andrea-sacchetto <74718356+Andrea46-pixel@users.noreply.github.com>
2022-11-08 17:16:49 +0100

    LAB_1

---
+++ LAB1_11-10-2022/es_1.7/Faces.java
@@ -0,0 +1,12 @@
+public class Faces{
+
+   public static void main(String[] args){
+   
+      System.out.println("///|||///");
+      System.out.println("(| o o |)");
+      System.out.println("(   v   )");
+      System.out.println("( < _ > )");
+      System.out.println("<_______>");
+   }
+
+}
---
+++ LAB1_11-10-2022/es_1.7/MyFaces.java
@@ -0,0 +1,10 @@
+public class MyFaces{
+
+public static void main(String[] args){
+        System.out.println(" ///|||///     ###|||###       %%%%%%");
+        System.out.println("(| O   O |)   (| O   O |)    ((~~~~~~))");
+        System.out.println(" (   V   )     |   8   |      ))^  ^(( ");
+        System.out.println("(  < _ >  )   (  VwwwV  )    ((  ..  ))");
+        System.out.println(" <_______>     <_______>      )) -- (( ");
+    }
+}
